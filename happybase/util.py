56fafa51f14ce4ab7830f7d07314ebc11736daf7
nikita.timoshin <nikita.timoshin@econophysica.com>
2018-06-28 12:06:33 +0700

    updated to be consistent with wbolster/happybase

--- happybase/util.py
+++ happybase/util.py
@@ -5,15 +5,15 @@ These functions are not part of the public API.
 """
 
 import re
-import six
 
+import six
 from six.moves import range
 
 CAPITALS = re.compile('([A-Z])')
 
 
 try:
-    # Python 2.7
+    # Python 2.7 and up
     from collections import OrderedDict
 except ImportError:
     try:
@@ -48,30 +48,13 @@ def pep8_to_camel_case(name, initial=False):
 
 def thrift_attrs(obj_or_cls):
     """Obtain Thrift data type attribute names for an instance or class."""
-    return [v[2] for v in obj_or_cls.thrift_spec[1:]]
+    return [v[1] for v in obj_or_cls.thrift_spec.values()]
 
 
 def thrift_type_to_dict(obj):
     """Convert a Thrift data type to a regular dictionary."""
     return dict((camel_case_to_pep8(attr), getattr(obj, attr))
                 for attr in thrift_attrs(obj))
-
-
-def bytes_increment(b):
-    """Increment and truncate a byte string (for sorting purposes)
-    This functions returns the shortest string that sorts after the given
-    string when compared using regular string comparison semantics.
-    This function increments the last byte that is smaller than ``0xFF``, and
-    drops everything after it. If the string only contains ``0xFF`` bytes,
-    `None` is returned.
-    """
-    assert isinstance(b, six.binary_type)
-    b = bytearray(b)  # Used subset of its API is the same on Python 2 and 3.
-    for i in range(len(b) - 1, -1, -1):
-        if b[i] != 0xff:
-            b[i] += 1
-            return bytes(b[:i+1])
-    return None
 
 
 def ensure_bytes(str_or_bytes, binary_type=six.binary_type,
@@ -84,3 +67,22 @@ def ensure_bytes(str_or_bytes, binary_type=six.binary_type,
     raise TypeError(
         "input must be a text or byte string, got {}"
         .format(type(str_or_bytes).__name__))
+
+
+def bytes_increment(b):
+    """Increment and truncate a byte string (for sorting purposes)
+
+    This functions returns the shortest string that sorts after the given
+    string when compared using regular string comparison semantics.
+
+    This function increments the last byte that is smaller than ``0xFF``, and
+    drops everything after it. If the string only contains ``0xFF`` bytes,
+    `None` is returned.
+    """
+    assert isinstance(b, six.binary_type)
+    b = bytearray(b)  # Used subset of its API is the same on Python 2 and 3.
+    for i in range(len(b) - 1, -1, -1):
+        if b[i] != 0xff:
+            b[i] += 1
+            return bytes(b[:i+1])
+    return None
